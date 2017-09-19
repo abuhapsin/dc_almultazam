9da6032715467a3c72bd4be3cca2f2eebc1a623c
abuhapsin <abuhapsin@gmail.com>
2017-09-19 15:54:42 +0700

    master awal

---
+++ application/views/login_page.php
@@ -0,0 +1,44 @@
+<div class="wrapper-page">
+    <span class="text-center">
+      <img src="<?php echo base_url('assets/images/dclogo.png');?>"/>
+    </span>
+    <div class="panel panel-color panel-pages">
+        <div class="panel-body">
+            <form class="cmxform form-horizontal tasi-form" id="signupForm" method="get" action="#" novalidate="novalidate">
+                <div class="alert alert-warning alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button> Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="#" class="alert-link">Alert Link</a>.
+                </div>
+                <div class="form-group ">
+                    <div class="col-xs-12">
+                        <input class="form-control input-lg " name="pengguna" type="text" required="" placeholder="Nama Pengguna">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-xs-12">
+                        <input class="form-control input-lg" type="password" required="" placeholder="Kata Sandi">
+                    </div>
+                </div>
+
+                <div class="form-group ">
+                    <div class="col-xs-12">
+                        <div class="checkbox checkbox-success">
+                            <input id="checkbox-signup" type="checkbox">
+                            <label for="checkbox-signup">
+                                    Remember me
+                                </label>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="form-group text-center m-t-40">
+                    <div class="col-xs-12">
+                        <button class="btn btn-success btn-lg w-lg waves-effect waves-light" type="submit">Masuk</button>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+
+    </div>
+</div>
---
+++ application/views/theme/scriptload.php
@@ -0,0 +1,23 @@
+    	<script>
+            var resizefunc = [];
+        </script>
+    	<script src="<?php echo base_url('assets/js/jquery.min.js');?>"></script>
+        <script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
+        <script src="<?php echo base_url('assets/js/waves.js');?>"></script>
+        <script src="<?php echo base_url('assets/js/wow.min.js');?>"></script>
+        <script src="<?php echo base_url('assets/js/jquery.nicescroll.js');?>" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/js/jquery.scrollTo.min.js');?>"></script>
+        <script src="<?php echo base_url('assets/assets/jquery-detectmobile/detect.js');?>"></script>
+        <script src="<?php echo base_url('assets/assets/fastclick/fastclick.js');?>"></script>
+        <script src="<?php echo base_url('assets/assets/jquery-slimscroll/jquery.slimscroll.js');?>"></script>
+        <script src="<?php echo base_url('assets/assets/jquery-blockui/jquery.blockUI.js');?>"></script>
+
+
+        <!-- CUSTOM JS -->
+        <script src="<?php echo base_url('assets/js/jquery.app.js');?>"></script>
+
+        <!--form validation-->
+        <script type="text/javascript" src="<?php echo base_url('assets/assets/jquery.validate/jquery.validate.min.js');?>"></script>
+
+        <!--form validation init-->
+        <script src="<?php echo base_url('assets/assets/jquery.validate/form-validation-init.js');?>"></script>
